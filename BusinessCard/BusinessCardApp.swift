40be1a84b40e8c0e147a9d43f068d38899281762
jeffreyvanelderen <jeffrey.vanelderen@acagroup.be>
2024-06-10 12:22:07 +0200

    Initial Commit

---
+++ BusinessCard/BusinessCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  BusinessCardApp.swift
+//  BusinessCard
+//
+//  Created by Jeffrey Vanelderen on 10/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct BusinessCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
